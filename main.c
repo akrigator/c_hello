1afea01acd5a83a679d7835321e9ee4835568164
Renat Gabdulhakov <renat.gabdulhakov@harman.com>
2016-09-12 16:44:41 +0300

    config and build

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 int main (int argc, char *argv[])
 {
-	if (argc == 1)
-	{
-		printf("Hello, world!\n");
-		return 0;
-	}
-	else 
-	{
-		return argc - 1;
-	}
+	printf("Hello, world!\n");
+	return 0;
 }
