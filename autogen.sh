790742185d8c9d406711a1031ef4bc4144669da3
Renat Gabdulhakov <renat.gabdulhakov@harman.com>
2016-09-12 18:08:20 +0300

    Fix AUTOTOOL flag name mistake

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 echo "Generating configure files... may take a while."
-
-touch NEWS README AUTHORS ChangeLog
 autoreconf --install --force && \
 	echo "Preparing was successful if there was no error messages above." && \
 	echo "Now type:" && \
